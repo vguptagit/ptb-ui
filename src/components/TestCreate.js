f7d229c4faa663a8705bdc025f361214838d9dd4
EXCELINDIA\monisha.ks <monisha.ks@excelindia.com>
2024-01-18 16:34:29 +0530

    In these testcreate removed the title and submit button

--- src/components/TestCreate.js
+++ src/components/TestCreate.js
@@ -1,59 +1,26 @@
-import React, { useEffect, useRef, useState } from "react";
-import { FormattedMessage } from "react-intl";
+// TestCreate.jsx
+import React, { useEffect } from "react";
 import { useAppContext } from "../context/AppContext";
-import Test from "../entities/Test.Entity";
 import QuestionsBanksTips from './testTabs/QuestionsBanksTips/QuestionsBanksTips';
+import "./TestCreate.css"; // Import the CSS file
 
 const TestCreate = () => {
-    const { dispatchEvent, selectedTest } = useAppContext();
-    const [newTest, setNewTest] = useState(new Test());
-    const inputRef = useRef(null);
+    const { selectedTest } = useAppContext();
 
     console.log('TestCreate on load');
 
-    // Step 1: Initialize State
-    const [formData, setFormData] = useState(new Test());
-    // Step 2: Handle Form Input Changes
-    const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        setFormData((prevData) => ({
-            ...prevData,
-            [name]: value,
-        }));
-    };
-
-    // Step 4: Handle Form Submission
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        // You can now use the formData state or send it to a server
-        console.log('Form data submitted:', formData);
-        if (formData.title) {
-            dispatchEvent("ADD_TEST", { test: formData });
-            setNewTest(new Test());
-        }
-    };
     useEffect(() => {
         console.log('TestCreate useEffect', selectedTest);
-    }, [])
+    }, [selectedTest]);
+
     return (
-        <div className="test-container p-1">
+        <div className="test-container">
             <div>
-                <h3>
-                    Test Create
-                </h3>
-            </div>
-            <div>
-                {selectedTest.title}
-                <form onSubmit={handleSubmit}>
-                    <label>
-                        Title:
-                        {/* <input type="text" name="title" value={selectedTest.title} onChange={handleInputChange} /> */}
-                        <input type="text" name="title" value={selectedTest.title} onChange={handleInputChange} ref={inputRef} />
-                    </label>
-                     
-                    <br />
-                    <button type="submit" className="m-1">Submit</button>
-                </form>
+                <label>
+                    Test Name
+                    <input type="text" name="title" placeholder="Enter Title" value={selectedTest.title} disabled />
+                </label>
+                <br />
                 <QuestionsBanksTips />
             </div>
         </div>
