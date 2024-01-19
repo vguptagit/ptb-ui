ef771f130a41bfffb4bf490f1fb16ffc74c1e6e9
nikitha.sy <nikitha.sy@excelindia.com>
2024-01-19 15:08:23 +0530

    UI discipline changes

--- src/pages/Discipline.js/Discipline.js
+++ src/pages/Discipline.js/Discipline.js
@@ -2,12 +2,11 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import { FormattedMessage } from 'react-intl';
 import './Discipline.css';
+import Loader from "../../components/common/loader/Loader";
 
 const LeftContent = () => {
   return (
     <div className="left-content">
-     
-    
       <ul>
         <li><FormattedMessage id="discipline.steps.3" /></li>
         <li><FormattedMessage id="discipline.steps.4" /></li>
@@ -23,6 +22,7 @@ const Discipline = () => {
   const [allData, setAllData] = useState([]);
   const [searchResults, setSearchResults] = useState([]);
   const [selectedItems, setSelectedItems] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const handleNext = () => {
     navigate("/home");
@@ -36,8 +36,10 @@ const Discipline = () => {
         const titles = data.map(item => item.title);
         setAllData(titles);
         setSearchResults(titles);
+        setLoading(false);
       } catch (error) {
         console.error('Error fetching data:', error);
+        setLoading(false);
       }
     };
 
@@ -74,43 +76,49 @@ const Discipline = () => {
 
   return (
     <div className="discipline-container">
-      <div className="top-container">
-      <p><b><FormattedMessage id="discipline.steps.2" /></b></p>
-        <button className="discipline btn btn-primary" onClick={handleNext}>Next</button>
-      </div>
-      <div className="discipline d-flex justify-content-between">
-        <LeftContent />
-        <div className="discipline search-container">
-          <div className="discipline input-group rounded">
-            <input
-              type="search"
-              width="100%"
-              className="discipline form-control rounded search-input"
-              placeholder="Search Discipline"
-              aria-label="Search"
-              aria-describedby="search-addon"
-              value={searchTerm}
-              onChange={handleSearch}
-            />
-            <div className="discipline input-group-append">
-              <span className="discipline input-group-text border-0" id="search-addon">
-                <i className="fas fa-search"></i>
-              </span>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <div className="top-container">
+            <p><b><FormattedMessage id="discipline.steps.2" /></b></p>
+            <button className="discipline btn btn-primary" onClick={handleNext}>Next</button>
+          </div>
+          <div className="discipline d-flex justify-content-between">
+            <LeftContent />
+            <div className="discipline search-container">
+              <div className="discipline input-group rounded">
+                <input
+                  type="search"
+                  width="100%"
+                  className="discipline form-control rounded search-input"
+                  placeholder="Search Discipline"
+                  aria-label="Search"
+                  aria-describedby="search-addon"
+                  value={searchTerm}
+                  onChange={handleSearch}
+                />
+                <div className="discipline input-group-append">
+                  <span className="discipline input-group-text border-0" id="search-addon">
+                    <i className="fas fa-search"></i>
+                  </span>
+                </div>
+              </div>
+              <ul className="discipline result-list mt-3">
+                {searchResults.map((item, index) => (
+                  <li
+                    key={index}
+                    className={`result-item ${selectedItems.includes(item) ? "selected" : ""}`}
+                    onClick={() => handleSelectItem(item)}
+                  >
+                    {item}
+                  </li>
+                ))}
+              </ul>
             </div>
           </div>
-          <ul className="discipline result-list mt-3">
-            {searchResults.map((item, index) => (
-              <li
-                key={index}
-                className={`result-item ${selectedItems.includes(item) ? "selected" : ""}`}
-                onClick={() => handleSelectItem(item)}
-              >
-                {item}
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
+        </>
+      )}
     </div>
   );
 };
