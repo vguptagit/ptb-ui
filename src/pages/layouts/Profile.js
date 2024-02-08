d0230f8997eb17a1ae127b2ad2aee8dd00f28528
Rohan.R <rohan.r@excelindia.com>
2024-02-08 17:18:51 +0530

    Tooltip and css changes

--- src/pages/layouts/Profile.js
+++ src/pages/layouts/Profile.js
@@ -3,44 +3,49 @@ import { Button } from "react-bootstrap";
 import { FormattedMessage } from "react-intl";
 import "./profile.css";
 import { useAuth } from "../../context/AuthContext";
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
 
 const Profile = () => {
   const { user, logout } = useAuth();
 
   return (
-    <NavDropdown
-      title={
-        <span>
-          <i className="bi bi-person-fill"></i> {user.name}
-        </span>
-      }
-      id="nav-dropdown"
-      className="profile-dropdown"
-    >
+  
+    <OverlayTrigger placement="bottom" overlay={<Tooltip id="profile-tooltip">Profile</Tooltip>}>
+      <NavDropdown
+        title={
+          <span>
+            <i className="bi bi-person-fill"></i> {user.name}
+          </span>
+        }
+        id="nav-dropdown"
+        className="profile-dropdown"
+      >
 
-      <NavDropdown.Item className="profile-name">{user.name}</NavDropdown.Item>
-      <NavDropdown.Item className="profile-email">{user.email}</NavDropdown.Item>
-      <NavDropdown.Divider />
-      <div className="settings-and-sign-out">
-        <div className="d-flex justify-content-center align-items-center">
-          <Button
-            variant="primary"
-            className="button-setting"
-          >
-            <FormattedMessage id="profile.setting" />
-          </Button>
+        <NavDropdown.Item className="profile-name">{user.name}</NavDropdown.Item>
+        <NavDropdown.Item className="profile-email">{user.email}</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <div className="settings-and-sign-out">
+          <div className="d-flex justify-content-center align-items-center">
+            <Button
+              variant="primary"
+              className="button-setting"
+            >
+              <FormattedMessage id="profile.setting" />
+            </Button>
+          </div>
+          <div className="d-flex justify-content-center align-items-center">
+            <Button
+              variant="primary"
+              className="button-signout"
+              onClick={logout}
+            >
+              <FormattedMessage id="profile.signout" />
+            </Button>
+          </div>
         </div>
-        <div className="d-flex justify-content-center align-items-center">
-          <Button
-            variant="primary"
-            className="button-signout"
-            onClick={logout}
-          >
-            <FormattedMessage id="profile.signout" />
-          </Button>
-        </div>
-      </div>
-    </NavDropdown>
+      </NavDropdown>
+    </OverlayTrigger>
   );
 };
 
