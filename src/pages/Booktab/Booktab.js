658d81d88c6af7d6bd88a9e25412a1bd3a26f1e5
EXCELINDIA\dharanitharan.t <dharanitharan.t@excelindia.com>
2024-02-15 12:51:58 +0530

    treeview changes

--- src/pages/Booktab/Booktab.js
+++ src/pages/Booktab/Booktab.js
@@ -1,38 +1,53 @@
 import React, { useEffect, useMemo, useState } from "react";
-import { useNavigate, useLocation } from 'react-router-dom';
-import { FormattedMessage } from 'react-intl';
-import './Booktab.css';
+import { useNavigate, useLocation } from "react-router-dom";
+import { FormattedMessage } from "react-intl";
+import "./Booktab.css";
 import Loader from "../../components/common/loader/Loader";
 import { getDisciplineBooks } from "../../services/book.service";
-// import { DndProvider } from "react-dnd";
-import { Tree, MultiBackend, getBackendOptions } from "@minoru/react-dnd-treeview";
-import { DndProvider, useDrag, useDrop } from 'react-dnd';
 
 const LeftContent = () => {
   return (
     <div className="left-content">
       <ul>
-        <li><FormattedMessage id="booktab.steps.2" /></li>
-        <li><FormattedMessage id="booktab.steps.3" /></li>
-        <li><FormattedMessage id="booktab.steps.4" /></li>
+        <li>
+          <FormattedMessage id="booktab.steps.2" />
+        </li>
+        <li>
+          <FormattedMessage id="booktab.steps.3" />
+        </li>
+        <li>
+          <FormattedMessage id="booktab.steps.4" />
+        </li>
       </ul>
     </div>
   );
 };
 
-const TreeNode = ({ node, onSelectItem }) => {
+const TreeNode = ({ node, onSelectItem, selectedItems }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const isSelected = selectedItems.includes(node.id);
+
   const handleNodeClick = () => {
-    if (!node.nodes || node.nodes.length === 0) {
+    setIsOpen(!isOpen);
+    if (!node.droppable || node.droppable.length === 0) {
       onSelectItem(node);
     }
   };
+
   return (
-    <div className={`tree-node ${!node.nodes ? 'innermost' : ''}`} onClick={handleNodeClick}>
-      {node.text}
-      {node.nodes && node.nodes.length > 0 && (
+    <div className={`tree-node ${isSelected ? "selected" : ""}`}>
+      <div className="tree-node-header" onClick={handleNodeClick}>
+        {node.text}
+      </div>
+      {isOpen && node.nodes && node.nodes.length > 0 && (
         <div className="nested-nodes">
           {node.nodes.map((childNode) => (
-            <TreeNode key={childNode.id} node={childNode} onSelectItem={onSelectItem} />
+            <TreeNode
+              key={childNode.id}
+              node={childNode}
+              onSelectItem={onSelectItem}
+              selectedItems={selectedItems}
+            />
           ))}
         </div>
       )}
@@ -44,43 +59,33 @@ const TreeView = ({ searchTerm, selectedItems, onSelectItem, treeData }) => {
   const filterNodes = (nodes, term) => {
     const filteredNodes = nodes.filter((node) => {
       const isMatch = node.text.toLowerCase().includes(term.toLowerCase());
-  
-   
       if (node.nodes && node.nodes.length > 0) {
-     
         node.nodes = filterNodes(node.nodes, term);
-  
-    
         if (node.nodes && node.nodes.length > 0) {
           return true;
         }
       }
-  
       return isMatch;
     });
-  
     return filteredNodes;
   };
-  
 
   const filteredTreeData = useMemo(() => {
     if (!searchTerm) {
       return treeData;
     }
-    
     return filterNodes(treeData, searchTerm);
   }, [searchTerm, treeData]);
-
-  const handleNodeClick = (node) => {
-    if (!node.droppable || node.droppable.length === 0) {
-      onSelectItem(node);
-    }
-  };
 
   return (
     <div className="treeview">
       {filteredTreeData.map((node) => (
-        <TreeNode key={node.id} node={node} onSelectItem={handleNodeClick} />
+        <TreeNode
+          key={node.id}
+          node={node}
+          onSelectItem={onSelectItem}
+          selectedItems={selectedItems}
+        />
       ))}
     </div>
   );
@@ -113,36 +118,36 @@ const Booktab = ({ onDropNode }) => {
         let newData = [];
         for (const setofItem of selectedItems) {
           const data = await getDisciplineBooks(setofItem);
-          console.log("Fetched data:", data); 
-          const formattedData = data.map(item => ({
+          const formattedData = data.map((item) => ({
             id: item.guid,
             text: `${item.discipline} - ${item.title}`,
             droppable: true,
-            nodes: item.nodes && item.nodes.map(node => ({
-              id: node.guid,
-              text: node.title,
-              droppable: true,
-              nodes: node.nodes && node.nodes.map(innerNode => ({
-                id: innerNode.guid,
-                text: innerNode.title,
-                droppable: false,
+            nodes:
+              item.nodes &&
+              item.nodes.map((node) => ({
+                id: node.guid,
+                text: node.title,
+                droppable: true,
+                nodes:
+                  node.nodes &&
+                  node.nodes.map((innerNode) => ({
+                    id: innerNode.guid,
+                    text: innerNode.title,
+                    droppable: false,
+                  })),
               })),
-            })),
           }));
           newData = [...newData, ...formattedData];
         }
-        console.log("New data:", newData); 
         setTreeData(newData);
-        console.log("treedata", treeData)
         setLoading(false);
       } catch (error) {
-        console.error('Error fetching discipline books:', error);
+        console.error("Error fetching discipline books:", error);
         setLoading(false);
       }
     };
     fetchData();
   }, [selectedItems]);
-
 
   const handleNext = () => {
     navigate("/home");
@@ -156,43 +161,23 @@ const Booktab = ({ onDropNode }) => {
     const term = e.target.value;
     setSearchTerm(term);
     const inputWidth = Math.max(200, e.target.scrollWidth);
-    document.querySelector('.search-input').style.minWidth = inputWidth + 'px';
+    document.querySelector(".search-input").style.minWidth = inputWidth + "px";
   };
 
   const handleSelectItem = (node) => {
     if (!node.droppable || node.droppable.length === 0) {
       setSelectedItems((prevSelectedItems) => {
         if (prevSelectedItems.includes(node.id)) {
-          const newSelectedItems = prevSelectedItems.filter((selectedItem) => selectedItem !== node.id);
-          console.log("Deselected:", node.id);
-          console.log("Selected items:", newSelectedItems);
-          return newSelectedItems;
+          return prevSelectedItems.filter(
+            (selectedItem) => selectedItem !== node.id
+          );
         } else {
-          const newSelectedItems = [...prevSelectedItems, node.id];
-          console.log("Selected:", node.id);
-          console.log("Selected items:", newSelectedItems);
-          return newSelectedItems;
+          return [...prevSelectedItems, node.id];
         }
       });
     }
   };
-  const handleNodeClick = (node) => {
-    if (!node.nodes || node.nodes.length === 0) {
-      alert(node.title);
-    }
-  };
 
-  const renderNode = (node) => (
-    <div key={node.id}>
-      <div
-        className={node.droppable ? 'droppable-node' : 'regular-node'}
-        onClick={() => handleNodeClick(node)}
-      >
-        {node.text}
-      </div>
-      {node.nodes && node.nodes.map((childNode) => renderNode(childNode))}
-    </div>
-  );
   return (
     <div className="booktab-container">
       {loading ? (
@@ -200,9 +185,15 @@ const Booktab = ({ onDropNode }) => {
       ) : (
         <>
           <div className="top-container">
-            <h4><FormattedMessage id="booktab.steps.1" /></h4>
-            <button className="booktab btn btn-secondary " onClick={handleBack}>Back</button>
-            <button className="booktab btn btn-primary" onClick={handleNext} >Next</button>
+            <h4>
+              <FormattedMessage id="booktab.steps.1" />
+            </h4>
+            <button className="booktab btn btn-secondary " onClick={handleBack}>
+              Back
+            </button>
+            <button className="booktab btn btn-primary" onClick={handleNext}>
+              Next
+            </button>
           </div>
           <div className="booktab d-flex justify-content-between">
             <LeftContent />
@@ -219,7 +210,10 @@ const Booktab = ({ onDropNode }) => {
                   onChange={handleSearch}
                 />
                 <div className="discipline input-group-append">
-                  <span className="discipline input-group-text border-0" id="search-addon">
+                  <span
+                    className="discipline input-group-text border-0"
+                    id="search-addon"
+                  >
                     <i className="fas fa-search"></i>
                   </span>
                 </div>
@@ -231,7 +225,6 @@ const Booktab = ({ onDropNode }) => {
                   searchTerm={searchTerm}
                   treeData={treeData}
                 />
-
               </ul>
             </div>
           </div>
