55d6d13bb94149d1a19d8c93eb8fc277b37f638d
EXCELINDIA\viresh.gupta <viresh.gupta@excelindia.com>
2024-02-23 10:25:51 +0530

    search for books nodes

--- src/components/AddQuestionsBanks.js
+++ src/components/AddQuestionsBanks.js
@@ -1,28 +1,14 @@
 import React, { useState,useEffect } from 'react';
 import { Button } from 'react-bootstrap';
 import { FormattedMessage } from 'react-intl';
-import SearchBox from './SearchBox/SearchBox';
+
 const QuestBanks = () => {
-  const [selectedItems, setSelectedItems] = useState([]);
+  
   useEffect(() => {
     document.title = "Question Banks";
   }, []);
-
-
-  
-  const handleSelectItem = (item) => {
-    setSelectedItems([...selectedItems, item]); 
-  };
-
-
   return (
     <>
-
-    <SearchBox
-        selectedItems={selectedItems}
-        handleSelectItem={handleSelectItem}
-      />
-    
       <div className="button-container">
           <Button className="color-black" variant="outline-light" Add Questions Banks>
             <i className="fa-solid fa-plus"></i>&ensp;
