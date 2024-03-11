5ab90d2698f397e621c8a3a6560c7cf134dc1f2f
Rohan.R <rohan.r@excelindia.com>
2024-03-11 14:50:35 +0530

    MT-3941 Your Test tabs update rendering new

--- src/pages/TestsPage.js
+++ src/pages/TestsPage.js
@@ -1,40 +1,15 @@
-import TestFolder from "../components/AddTestFolder";
-import React, { useEffect, useState } from 'react';
-import { getRootTestFolders } from '../services/testfolder.service';
-import { getFolderTests } from "../services/testcreate.service";
-import Toastify from '../components/common/Toastify';
+import React, { useEffect } from 'react';
+import Testing from '../components/common/Testing'
+import TestFolder from '../components/AddTestFolder';
 
 const Tests = () => {
-  const [rootFolders, setRootFolders] = useState([]);
-  const [doReload, setDoReload] = useState(false);
-
-  useEffect(() => {
-    document.title = "Your Tests";
-  }, []);
-
-  useEffect(() => {
-    Promise.all([getRootTestFolders(), getFolderTests(null)])
-        .then(([rootFoldersResponse, folderTestsResponse]) => {
-            // Assuming both responses are arrays
-            const combinedData = [...rootFoldersResponse, ...folderTestsResponse];
-            setRootFolders(combinedData);
-        })
-        .catch((error) => {
-            console.error('Error getting root folders or folder tests:', error);
-            if (error?.message?.response?.request?.status === 409) {
-                Toastify({ message: error.message.response.data.message, type: 'error' });
-            } else {
-                Toastify({ message: 'Failed to get root folders or folder tests', type: 'error' });
-            }
-        });
-}, [doReload]);
-
-
-  return (
-    <div className="p-2">
-      <TestFolder rootFolders={rootFolders} doReload={doReload} setDoReload={setDoReload} />
-    </div>
-  );
-};
-
+    useEffect(() => {
+        document.title = "Your Tests";
+      }, []);
+    return (
+        <>
+            <TestFolder/>
+        </>
+    );
+}
 export default Tests;
--- src/pages/tree-view-test-folders/TreeView.js
+++ src/pages/tree-view-test-folders/TreeView.js
@@ -1,243 +1,226 @@
 import React, { useState, useEffect } from "react";
 import { Tree } from "@minoru/react-dnd-treeview";
 import "./TreeView.css";
+import { getUserTestFolders } from "../../services/testfolder.service";
 import { getFolderTests } from "../../services/testcreate.service";
 
-const DraggableNode = ({
-  node,
-  onToggle,
-  folderName,
-  depth,
-  isOpen,
-  selectedFolderId,
-  handleFolderSelect,
-  handleFolderSelectOnParent
-}) => {
-  const [selectedNodeId, setSelectedNodeId] = useState(null);
-
-  const handleEditFolder = (folderTitle) => {
-    if (handleFolderSelect) {
-      handleFolderSelect(folderTitle);
-    }
-  };
-
-   const handleNodeClick = (e) => {
-    // If the clicked node is different from the currently selected node
-    if (selectedNodeId !== node.data.guid) {
-      setSelectedNodeId(node.data.guid); // Select the clicked node
-      console.log("Selected Node ID:", node.data.guid);
-    } else {
-      // If the clicked node is the same as the currently selected node
-      setSelectedNodeId(null); // Deselect the clicked node
-    }
-    onToggle(); // Toggle node state
-    // handleFolderSelectOnParent(node);
-    e.preventDefault();
-    e.stopPropagation();
-  };
-
-  const handleDeleteFolder = (folderTitle) => {
-    console.log("Delete folder:", folderTitle);
-  };
-
-  return (
-    <div
-      className={`tree-nodes ${selectedFolderId === node.data.guid ? 'selected' : ''}`}
-      style={{ marginInlineStart: depth * 10 }}
-    >
-      <div className="node-name">
-        {node.data.sequence !== undefined && (
-          <span>
-            {isOpen ? (
-              <i className="bi bi-caret-down-fill"
-                onClick={handleNodeClick}
-              ></i>
-            ) : (
-              <i className="bi bi-caret-right-fill"
-                onClick={handleNodeClick}
-              ></i>
-            )}
-          </span>
-        )}
-        {node.text}
-      </div>
-      <div>
-      {folderName === node.text && (
-        <button
-          className={`edit-btn ${node.data.sequence === undefined ? 'disabled' : ''} ${selectedNodeId === node.data.guid ? '' : 'selected'}`}
-          onClick={node.data.sequence === undefined ? undefined : () => handleEditFolder(node.text)}
-          disabled={node.data.sequence === undefined}
-        >
-          <i className="bi bi-pencil-fill"></i>
-      </button>
-      )}
-      {folderName !== node.text && (
-        <button
-          className={`edit-btn ${node.data.sequence === undefined ? 'disabled' : ''} ${selectedNodeId === node.data.guid ? '' : 'selected'}`}
-          onClick={node.data.sequence === undefined ? undefined : () => handleEditFolder(node.text)}
-          disabled={node.data.sequence === undefined}
-        >
-          <i className="bi bi-pencil-fill"></i>
-        </button>
-      )}
-      {folderName === node.text && (
-        <button
-          className="delete-btn"
-          onClick={() => handleDeleteFolder(node.text)}
-        >
-          <i className="bi bi-trash"></i>
-        </button>
-      )}
-      {folderName !== node.text && (
-        <button
-        className="delete-btn"
-        onClick={() => handleDeleteFolder(node.text)}
-      >
-        <i className="bi bi-trash"></i>
-      </button>
-      )}
-      </div>
-    </div>
-  );
-};
-
-function TreeView({ testFolders, folderName, onNodeUpdate, handleFolderSelect ,handleFolderSelectOnParent,}) {
+function TreeView({ 
+  onFolderSelect,
+  onNodeUpdate,
+  folders,
+  rootFolderGuid,
+  selectedFolderGuid,
+}) {
   const [treeData, setTreeData] = useState([]);
-  const [selectedFolderId, setSelectedFolderId] = useState(null);
-  console.log("selectedfolderID",selectedFolderId);
+  const [selectedFolder, setSelectedFolder] = useState(null);
+  const [isDragging, setIsDragging] = useState(false);
+
+  const fetchChildFolders = async (parentNode) => {
+    try {
+      if(!parentNode.children && parentNode.data.guid !== selectedFolderGuid) {
+        const childFolders = await getUserTestFolders(
+          parentNode.data.guid
+        );
+        const childTests = await getFolderTests(
+          parentNode.data.guid
+        );
+        const childNodes = [
+          ...childFolders.map((childFolder, childIndex) => ({
+            id: `${parentNode.id}.${childIndex + 1}`,
+            parent: parentNode.id,
+            droppable: true,
+            text: childFolder.title,
+            data: {
+              guid: childFolder.guid,
+              sequence: childFolder.sequence,
+            },
+          })),
+          ...childTests.map((childTest, childIndex) => ({
+            id: `${parentNode.id}.${childIndex + 1}`,
+            parent: parentNode.id,
+            droppable: false,
+            text: childTest.title,
+            data: {
+              guid: childTest.guid,
+            },
+          })),
+        ];
+
+        const updatedTreeData = [...treeData];
+        const nodeIndex = updatedTreeData.findIndex(
+          (n) => n.id === parentNode.id
+        );
+
+        const existingChildNodes = updatedTreeData
+          .slice(nodeIndex + 1)
+          .filter((node) => node.parent === parentNode.id);
+        if (existingChildNodes.length === 0) {
+          updatedTreeData.splice(nodeIndex + 1, 0, ...childNodes)
+          setTreeData(updatedTreeData);
+        }
+      }
+    } catch (error) {
+      console.error("Error fetching child question folders:", error);
+    }
+  };
+
   useEffect(() => {
-    sessionStorage.setItem('selectedFolderId', JSON.stringify(selectedFolderId));
-  }, [selectedFolderId]);
-  const handleDrop = (newTree, { dragSource, dropTarget, destinationIndex }) => {
-    setTreeData(newTree);
+    if (folders && folders.length > 0) {
+      const updatedTreeData = folders.map((folder, index) => ({
+        id: folder.guid,
+        parent: 0,
+        droppable: folder.parentId ? true : false,
+        text: folder.title,
+        data: {
+          guid: folder.guid,
+          sequence: folder.sequence,
+        },
+      }));
+      setTreeData(updatedTreeData);
+    }
+  }, [folders]);
+
+  const handleDrop = async (newTree, { dragSource, dropTarget }) => {
+    let parentId;
+
+    if(dropTarget && dropTarget.data) {
+      parentId = dropTarget.data.guid;
+    } else {
+      parentId = rootFolderGuid;
+    }
+
     const nodeToBeUpdated = {
       guid: dragSource.data.guid,
-      parentId: dropTarget === undefined ? 0 : dropTarget.data.guid,
-      sequence: dropTarget === undefined ? getNextSequenceWithoutParentId(dragSource) : getNextSequenceForParentFolderId(dropTarget.data.guid),
-      extUserId: window.piSession.userId(),
+      parentId: parentId,
+      sequence: dropTarget ? dropTarget.data.sequence : 0,
+      title: dragSource.text,
+    };
+
+    try{
+      const childFolders = await getUserTestFolders(parentId);
+      const childNodes = childFolders.map((childFolder, index) => ({
+        id: `${parentId}.${index + 1}`,
+        parent: parentId,
+        droppable: true,
+        text: childFolder.title,
+        data: {
+          guid: childFolder.guid,
+          sequence: childFolder.sequence,
+        },
+      }));
+      const parentIndex = newTree.findIndex((node) => node.id === parentId);
+      const isChildNode = parentId.toString().includes(".");
+      const updatedParentIndex = isChildNode ? parentIndex - 1 : parentIndex;
+      const updatedTreeData = [...newTree];
+      updatedTreeData.splice(updatedParentIndex + 1, 0, ...childNodes);
+      setTreeData(updatedTreeData);
+    } catch (error) {
+      console.error("Error fetching child question folders:", error);
     }
     onNodeUpdate(nodeToBeUpdated);
   };
 
-  
-  function getIndexByParentGuid(parentGuid) {
-    return testFolders.findIndex(ele => ele.guid === parentGuid);
-  }
-
-  function getNextSequenceForParentFolderId(parentFolderId) {
-    let maxSequence = 0;
-    for (const folder of testFolders) {
-      if (folder.parentId === parentFolderId && folder.sequence > maxSequence) {
-        maxSequence = folder.sequence;
+  const handleEditFolder = (folderTitle) => {
+    console.log("Edit folder:", folderTitle);
+    if (selectedFolder === folderTitle) {
+      setSelectedFolder(null);
+      if (onFolderSelect) {
+        onFolderSelect("");
       }
-    }
-    return maxSequence + 1;
-  }
-
-  function getNextSequenceWithoutParentId(dragSource) {
-    return dragSource.data.sequence;
-  }
-
-  const testDetails = async (guid) => {
-    try {
-      const folderTests = await getFolderTests(guid);
-
-      const titleNames = [];
-
-    // Loop through folderTests and save titlenames
-    for (const test of folderTests) {
-      // Assuming test object has a property called 'titlename'
-      if (test && test.title) {
-        titleNames.push(test.title);
+    } else {
+      if (onFolderSelect) {
+        onFolderSelect(folderTitle);
       }
+      setSelectedFolder(folderTitle);
     }
+  }
 
-    return titleNames;
-
-    } catch (error) {
-      console.error("Error fetching folder tests:", error);
-    }
+  const handleDragStart = () => {
+    setIsDragging(true);
   };
 
-  useEffect(() => {
-    if (testFolders && testFolders.length > 0) {
-      const biggestSequence = testFolders.reduce((maxSequence, folder) => {
-        return Math.max(maxSequence, folder.sequence);
-    }, -Infinity);
-      const specifiedSequence = biggestSequence;
-      const folderNodes = testFolders.map((folder, index) => ({
-        id: index + 1, 
-        parent: getIndexByParentGuid(folder.parentId) !== 0 ? getIndexByParentGuid(folder.parentId) + 1 : 0,
-        droppable: true,
-        text: folder.title,
-        data: {
-            guid: folder.guid,
-            sequence: folder.sequence,
-        }
-    }));
-      folderNodes.sort((a, b) => a.data.sequence - b.data.sequence);
-      
-      const insertIndex = folderNodes.findIndex(node => node.data.sequence === specifiedSequence);
-      if (insertIndex !== -1) {
-        const movedFolders = folderNodes.splice(insertIndex);
-        folderNodes.unshift(...movedFolders);
-      }
+  const handleDragEnd = () => {
+    setIsDragging(false);
+  };
 
-      const fetchTestDetails = async () => {
-        for (const folder of testFolders) {
-            try {
-                const titleNames = await testDetails(folder.guid);
-                for (const testName of titleNames) {
-                  folderNodes.push({
-                        id: `${folder.guid}-${testName}`, // Unique id for test node
-                        parent: getIndexByParentGuid(folder.guid) !== 0 ? getIndexByParentGuid(folder.guid) + 1 : 0, // Parent id is the folder's guid
-                        droppable: false,
-                        text: testName,
-                        data: {
-                            testName: testName,
-                        },
-                    });
-                }
-            } catch (error) {
-                console.error("Error fetching folder tests:", error);
-            }
-        }
-        const folderNodesFiltered = folderNodes.filter(node => node.text !== null);
-        setTreeData(folderNodesFiltered);
-    };
-    fetchTestDetails();
+  const handleMouseDown = () => {
+    setIsDragging(true);
+  };
 
-    }
-  }, [testFolders]);
+  const handleMouseUp = () => {
+    setIsDragging(false);
+  };
+  
+  const handleDeleteFolder = (folderTitle) => {
+    console.log("Delete folder:", folderTitle);
+  };
+  
 
   return (
-    <>
-      <div className="treeview">
-        <Tree
-          tree={treeData}
-          rootId={0}
-          render={(node, { depth, isOpen, onToggle }) => {
-            return (
-                <DraggableNode
-                    node={node}
-                    isOpen={isOpen}
-                    folderName={folderName}
-                    depth={depth}
-                    onToggle={onToggle}
-                    selectedFolderId={selectedFolderId}
-                    handleFolderSelect={handleFolderSelect}
-                    handleFolderSelectOnParent={handleFolderSelectOnParent}
-                />
-            );
-        }}
-          dragPreviewRender={(monitorProps) => (
-            <div>{monitorProps.item.node?.text}</div>
-          )}
-          onDrop={handleDrop}
-          sort={false}
-        />
-      </div>
-    </>
+    <div
+      className={`treeview ${isDragging ? "grabbing" : ""}`}
+      onMouseDown={handleMouseDown}
+      onMouseUp={handleMouseUp}
+    >
+      <Tree
+        tree={treeData}
+        rootId={0}
+        render={(node, { isOpen, onToggle }) => (
+          <div className="tree-node">
+            {node.droppable && (
+              <span
+                onClick={() => {
+                  if (
+                    !isOpen &&
+                    (!node.children || node.children.length === 0)
+                  ) {
+                    fetchChildFolders(node);
+                  }
+                  onToggle();
+                }}
+                className="custom-caret"
+              >
+                {isOpen ? (
+                  <i className="fa fa-caret-down"></i>
+                ) : (
+                  <i className="fa fa-caret-right"></i>
+                )}
+              </span>
+            )}
+            {node.text}
+            {selectedFolder === node.text && (
+              <button
+                className="editbutton selected"
+                onClick={() => handleEditFolder(node.text)}
+              >
+                <i className="bi bi-pencil-fill"></i>
+              </button>
+            )}
+            {selectedFolder !== node.text && (
+              <button
+                className={`editbutton ${node.droppable === false ? 'disabled' : ''}`}
+                onClick={() => handleEditFolder(node.text)}
+              >
+                <i className="bi bi-pencil-fill"></i>
+              </button>
+            )}
+            <button
+              className="deletebutton"
+              onClick={() => handleDeleteFolder(node.text)}
+            >
+              <i className="bi bi-trash"></i>
+            </button>
+          </div>
+        )}
+        dragPreviewRender={(monitorProps) => (
+          <div className="custom-drag-preview">{monitorProps.item.text}</div>
+        )}
+        onDrop={handleDrop}
+        dragPreviewClassName="custom-drag-preview"
+        onDragStart={handleDragStart}
+        onDragEnd={handleDragEnd}
+      />
+    </div>
   );
 }
 
