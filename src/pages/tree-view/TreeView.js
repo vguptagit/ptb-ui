773762900483be72ce7a18d39a2a1bead85ea9d3
ganapati.bhagwat <ganapati.bhagwat@excelindia.com>
2024-04-01 13:23:21 +0530

    MT-3921 Question Bank - Render Question -True False, Multiple response , Multiple Choice

--- src/pages/tree-view/TreeView.js
+++ src/pages/tree-view/TreeView.js
@@ -345,7 +345,7 @@ function TreeView({ onDataUpdate, droppedNode, disciplines, searchTerm }) {
               questionNode={question}
               questionNodeIndex={index}
               qtiModel={qtiModel}
-              printView={1}
+              printView={3}
             />
           </div>
         );
@@ -356,7 +356,7 @@ function TreeView({ onDataUpdate, droppedNode, disciplines, searchTerm }) {
               questionNode={question}
               questionNodeIndex={index}
               qtiModel={qtiModel}
-              printView={1}
+              printView={3}
             />
           </div>
         );
@@ -367,7 +367,7 @@ function TreeView({ onDataUpdate, droppedNode, disciplines, searchTerm }) {
               questionNode={question}
               questionNodeIndex={index}
               qtiModel={qtiModel}
-              printView={1}
+              printView={3}
             />
           </div>
         );
@@ -378,7 +378,7 @@ function TreeView({ onDataUpdate, droppedNode, disciplines, searchTerm }) {
               questionNode={question}
               questionNodeIndex={index}
               qtiModel={qtiModel}
-              printView={1}
+              printView={3}
             />
           </div>
         );
@@ -389,7 +389,7 @@ function TreeView({ onDataUpdate, droppedNode, disciplines, searchTerm }) {
               questionNode={question}
               questionNodeIndex={index}
               qtiModel={qtiModel}
-              printView={1}
+              printView={3}
             />
           </div>
         );
@@ -400,7 +400,7 @@ function TreeView({ onDataUpdate, droppedNode, disciplines, searchTerm }) {
               questionNode={question}
               questionNodeIndex={index}
               qtiModel={qtiModel}
-              printView={1}
+              printView={3}
             />
           </div>
         );
