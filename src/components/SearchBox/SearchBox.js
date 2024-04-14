68b11f03a638077d5aa4228aab312fbbeb547f53
EXCELINDIA\sivaramakrishna.t <sivaramakrishna.t@excelindia.com>
2024-04-14 21:34:10 +0530

    fix: Question Bank Tree issues

--- src/components/SearchBox/SearchBox.js
+++ src/components/SearchBox/SearchBox.js
@@ -1,58 +1,38 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import { useDebounce } from '../../hooks/useDebounce';
+import './SearchBox.css';
 
-function SearchBox({ onSearch }) {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [searchResults, setSearchResults] = useState([]);
-  const debounceTimeoutRef = useRef();
-  // logic to fetch search results here
-  const handleSearch = (event) => {
-   
-    const value = event.target.value;
-    setSearchTerm(value)
-    if (debounceTimeoutRef.current) {
-      clearTimeout(debounceTimeoutRef.current);
-    }
-    debounceTimeoutRef.current = setTimeout(() => {
-      onSearch(value); // Call the onSearch prop function after the delay
-    }, 1000);
-    // onSearch(value); // Call the onSearch prop function
-  };
-  const handleSelectItem = (item) => {
-    // logic to handle item selection here
-  };
+function SearchBox({ searchText, onSearch }) {
+  const [searchTerm, setSearchTerm] = useState(searchText || '');
+  const debouncedSearch = useDebounce(searchTerm, 1000);
+
+  useEffect(() => {
+    onSearch(debouncedSearch);
+  }, [debouncedSearch]);
 
   return (
-    <div className="col-lg-7">
-      <div className="input-group rounded">
+    <div className='col-lg-7'>
+      <div className='input-group rounded'>
         <input
-          type="search"
-          width="100%"
-          className=" form-control rounded search-input"
-          placeholder="Search selected banks"
-          aria-label="Search"
-          aria-describedby="search-addon-noborder"
+          type='text'
+          width='100%'
+          className='form-control rounded'
+          placeholder='Search selected banks'
+          aria-label='Search'
+          aria-describedby='search-addon-noborder'
           value={searchTerm}
-          onChange={handleSearch}
+          onChange={event => setSearchTerm(event.target.value)}
         />
-        <div className="discipline input-group-append">
-        <span
-            className="input-group-text bg-transparent border-0"
-            id="search-addon-noborder"
-          >
+        <div className='input-group-append'>
+          <span className='input-group-icon border-0'>
+            {searchTerm ? (
+              <i className='fas fa-times pointer' onClick={() => setSearchTerm('')}></i>
+            ) : (
+              <i className='fas fa-search'></i>
+            )}
           </span>
         </div>
       </div>
-      <ul className="result-list mt-3">
-        {searchResults.map((item, index) => (
-          <li
-            key={index}
-            className="result-item"
-            onClick={() => handleSelectItem(item)}
-          >
-            {item}
-          </li>
-        ))}
-      </ul>
     </div>
   );
 }
--- src/pages/QuestionBanks.js
+++ src/pages/QuestionBanks.js
@@ -1,50 +1,47 @@
-import TreeView from "./tree-view/TreeView";
-import QuestBanks from "../components/AddQuestionbankfolder/AddQuestionsBanks";
-import { getUserDisciplines } from "../services/discipline.service";
-import React, { useState, useEffect } from "react";
-import SearchBox from "../components/SearchBox/SearchBox";
-import "./QuestionBanks.css"
+import TreeView from './tree-view/TreeView';
+import QuestBanks from '../components/AddQuestionbankfolder/AddQuestionsBanks';
+import { getUserDisciplines } from '../services/discipline.service';
+import React, { useState, useEffect } from 'react';
+import SearchBox from '../components/SearchBox/SearchBox';
+import './QuestionBanks.css';
+
 const QuestionBanks = () => {
   const [disciplineData, setDisciplineData] = useState([]);
   const [selectedItems, setSelectedItems] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState('');
 
-
-  const handleSelectItem = (item) => {
+  const handleSelectItem = item => {
     setSelectedItems([...selectedItems, item]);
   };
- 
+
   useEffect(() => {
     // Fetch data from the API using getAllBooks
     getUserDisciplines()
-      .then((data) => {
+      .then(data => {
         if (data) {
           const objCopy = [...data];
           setDisciplineData(objCopy);
         }
       })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
+      .catch(error => {
+        console.error('Error fetching data:', error);
       });
   }, []);
-  const handleSearch = (term) => {
-    console.log("term in questions bank is as follows", term);
+
+  const handleSearch = term => {
+    console.log('term in questions bank is as follows', term);
     setSearchTerm(term);
   };
+
   return (
-    <div className="pt-2">
-      <SearchBox
-        onSearch={handleSearch}
-        selectedItems={selectedItems}
-        handleSelectItem={handleSelectItem}
-      />
-      <div className="questionBank">
-        <QuestBanks 
-        />
+    <div className='pt-2'>
+      <SearchBox onSearch={handleSearch} />
+      <div className='questionBank'>
+        <QuestBanks />
       </div>
       {disciplineData.length > 0 && (
         <div>
-          <TreeView disciplines={disciplineData} searchTerm={searchTerm} />{" "}
+          <TreeView disciplines={disciplineData} searchTerm={searchTerm}/>{' '}
         </div>
       )}
     </div>
