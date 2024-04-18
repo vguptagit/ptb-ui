5e22dbfc4219cd5794e2fa97536fbc4ecdd6774d
EXCELINDIA\sivaramakrishna.t <sivaramakrishna.t@excelindia.com>
2024-04-18 23:49:11 +0530

    fix: prettier eol changes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,7 +13,6 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
-    'prettier/prettier': 'off',
     'react/jsx-no-target-blank': 'off',
     'react-hooks/exhaustive-deps': 'off',
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
